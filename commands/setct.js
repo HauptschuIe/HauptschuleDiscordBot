8e58a05b0f8b47d488ea3f029e97d8d083553cea
HauptschuIe <jw126@hdm-stuttgart.de>
2019-08-12 17:42:22 +0200

    Add permissions restriction

--- commands/setct.js
+++ commands/setct.js
@@ -1,16 +1,37 @@
+
+// set for saving people for cooldown
+const userUsedCommandRecently = new Set();
+
 module.exports = {
   name: 'setct',
   description: 'Rename/set channel topic',
 
   execute(message, args) {
 
-    if(!args[1]){
-      const topic = " ";
-      message.channel.setTopic('Uff')
-      message.channel.send(topic);
-    }else{
-      const topic = String(args[0]);
-      message.channel.setTopic(topic)
+    const cmonBruh = message.guild.emojis.find(emoji => emoji.name === "cmonBruh");
+    const topic = String(args[0]);
+
+    if (!message.member.roles.find(r => r.name === "Mods") && userUsedCommandRecently.has(message.author.id)){
+      return message.reply(`you still do not have permissions to prune messages ${cmonBruh}`);
+
+    } else if (!message.member.roles.find(r => r.name === "Mods") && !userUsedCommandRecently.has(message.author.id)) {
+      userUsedCommandRecently.add(message.author.id)
+      setTimeout(() => {
+      userUsedCommandRecently.delete(message.author.id)
+      }, 30000);
+      return message.reply('you do not have permissions to prune messages.');
+        }
+
+    if(!topic){
+      message.channel.setTopic(null).catch(err => {
+  			console.error(err);
+      });
+      return message.channel.send('Channel topic has been removed');
+
+    }else if (topic){
+      message.channel.setTopic(topic).catch(err => {
+  			console.error(err);
+      });
       return message.channel.send('Channel topic has been changed to ' + topic);
     }
 
