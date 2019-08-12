f54d3d311b203bac9470220feb30438c3b08bec5
HauptschuIe <jw126@hdm-stuttgart.de>
2019-08-12 17:24:17 +0200

    Fix error

--- commands/setct.js
+++ commands/setct.js
@@ -6,14 +6,15 @@ module.exports = {
 
     if(!args[1]){
       const topic = " ";
-
+      message.channel.setTopic('Uff')
+      message.channel.send(topic);
+      .catch(console.error);
     }else{
       const topic = String(args[0]);
+      message.channel.setTopic(topic)
+      .catch(console.error);
+      return message.channel.send('Channel topic has been changed to ' + topic);
     }
-
-    message.channel.setTopic(topic)
-    .catch(console.error);
-    return message.channel.send('Channel topic has been changed to ' + topic);
 
   },
 };
