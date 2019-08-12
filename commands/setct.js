03a2eda25783e4f219f9a52871d231da8655e8cb
HauptschuIe <jw126@hdm-stuttgart.de>
2019-08-12 17:04:58 +0200

    Add catch method

--- commands/setct.js
+++ commands/setct.js
@@ -1,11 +1,19 @@
 module.exports = {
-	name: 'setct',
-	description: 'Rename/set channel topic',
+  name: 'setct',
+  description: 'Rename/set channel topic',
 
-	execute(message, args) {
+  execute(message, args) {
 
-    message.channel.setTopic(args[1]);
-    return message.channel.send('Channel topic has been changed to ' + args[1]);
+    if(!args[1]){
+      const topic = "";
 
-	},
+    }else{
+      const topic = args[1];
+    }
+
+    message.channel.setTopic(topic)
+    .catch(console.error);
+    return message.channel.send('Channel topic has been changed to ' + topic);
+
+  },
 };
