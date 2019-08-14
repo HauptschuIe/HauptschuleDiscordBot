94208a0d808e5f8ad05aa98d54257c7ab349ae57
HauptschuIe <jw126@hdm-stuttgart.de>
2019-08-14 19:48:30 +0200

    Fix method

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ const set1 = new Set(["thats jew",
 "nigga",
 "niggar",
 "testword123"]);
+var iterator1 = set1.values();
 const set2 = new Set([
 	"golam",
 	"golaam",
@@ -92,38 +93,41 @@ clbot.on('message', message => {
 	if(message.content.toLowerCase().includes(wordToReactTo2)){
 		message.react(clbot.emojis.get("518878496546881557"));
 	}
+
 	// auto-ban users by message
-	if(set1.has(message.content.toLowerCase())){
-		const member = message.guild.member(message.author);
-		if(member){
-			member.ban({
-				reason: 'Message',
-			}).catch(err => {
-				console.error(err);
-			});
-			const channel = message.guild.channels.find(channel => channel.name === "united-antichess-security-council");
-			if(!channel) return;
-			channel.send(`${message.author} has been banned for saying "${message}"`)
+	for (var i = 0; i < set1.length; i++) {
+		if(message.content.toLowerCase().includes(iterator1.next().value)){
+			const member = message.guild.member(message.author);
+			if(member){
+				member.ban({
+					reason: 'Message',
+				}).catch(err => {
+					console.error(err);
+				});
+				const channel = message.guild.channels.find(channel => channel.name === "united-antichess-security-council");
+				if(!channel) return;
+				channel.send(`${message.author} has been banned for saying "${message}"`)			
+			}
+			}
+		};
+
+
+
+		if (!message.content.startsWith(PREFIX)) return;
+
+		let args = message.content.substring(PREFIX.length).split(" ");
+
+		const command = args.shift().toLowerCase();
+
+		if (!clbot.commands.has(command)) return;
+
+		try {
+			clbot.commands.get(command).execute(message, args);
+		} catch (error) {
+			console.error(error);
+			message.reply('there was an error trying to execute that command!');
 		}
-	}
+	});
 
-
-
-	if (!message.content.startsWith(PREFIX)) return;
-
-	let args = message.content.substring(PREFIX.length).split(" ");
-
-	const command = args.shift().toLowerCase();
-
-	if (!clbot.commands.has(command)) return;
-
-	try {
-		clbot.commands.get(command).execute(message, args);
-	} catch (error) {
-		console.error(error);
-		message.reply('there was an error trying to execute that command!');
-	}
-});
-
-// Log our bot in using the token from https://discordapp.com/developers/applications/me
-clbot.login(token);
+	// Log our bot in using the token from https://discordapp.com/developers/applications/me
+	clbot.login(token);
