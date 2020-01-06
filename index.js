a56b7c3c79a0709efa2d112487664fefc90be8d6
HauptschuIe <jw126@hdm-stuttgart.de>
2020-01-06 18:33:32 +0100

    fix commands path

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const set2 = new Set([
 const PREFIX = '!';
 
 clbot.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
