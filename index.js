d21ac9b0382edf3857af23c2def9a40a457305fa
HauptschuIe <jw126@hdm-stuttgart.de>
2020-01-06 18:54:58 +0100

    test

--- index.js
+++ index.js
@@ -36,31 +36,123 @@ const PREFIX = '!';
 clbot.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 
-for(const file of commandFiles){
-    const command = require(`./commands/${file}`);
- 
-    clbot.commands.set(command.name, command);
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	clbot.commands.set(command.name, command);
 }
- 
-clbot.on('ready', () => {
-    console.log("The bot is active and ready to go!");
+
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
+clbot.on('ready', () =>{
+	console.log('Abfahrt!');
+	clbot.user.setPresence({
+		game: {
+			name: 'Dank Music',
+			type: "LISTENING",
+		}
+	});
+})
+
+// when an user joins the server a welcome message will be sent
+clbot.on('guildMemberAdd', member =>{
+	const PogChamp = member.guild.emojis.find(emoji => emoji.name === "PogChamp");
+	let guild = member.guild;
+	var userCount = member.guild.memberCount;
+	const channel = member.guild.channels.find(channel => channel.name === "united-antichess-security-council");
+	if(!channel) return;
+
+	channel.send(`A new Movag appeared, ${member} this is already Movag no. ${userCount}! ${PogChamp}`)
+
+	// ban users having one of the Strings in set2 in their username
+	if(set2.has(member.user.tag)){
+		if(member){
+			member.ban({
+				reason: 'Nickname',
+			}).catch(err => {
+				console.error(err);
+			});
+			const channel = message.guild.channels.find(channel => channel.name === "united-antichess-security-council");
+			if(!channel) return;
+			channel.send(`${member} has been banned. Reason: Nickname`)
+		}
+	}
+
 });
- 
+
+
 clbot.on('message', message => {
-    let args = message.content.substring(PREFIX.length).split(" ");
- 
-    switch (args[0]) {
- 
-        case "avatar":
-            clbot.commands.get('avatar').execute(message, args);
-        break;
- 
-        case "hello":
-            clbot.commands.get('hello').execute(message, args);
-        break;
-    }
- 
-});
+
+	if (message.author.bot) return;
+
+	const wordToReactTo1 = "movag";
+	const wordToReactTo2 = "metin";
+
+	// bot reacts/replies to messages containing the words above
+	if(message.content.toLowerCase().includes(wordToReactTo1)){
+		message.react(clbot.emojis.get("639186943590006825"));
+	}
+	if(message.content.toLowerCase().includes(wordToReactTo2)){
+		message.react(clbot.emojis.get("639186943590006825"));
+	}
+
+	// delete messages in #story containing more than one word
+	const StoryChannel = message.guild.channels.get("id", 453514739595739136)
+	const cmonBruh = message.guild.emojis.find(emoji => emoji.name === "cmonBruh");
+	if(!StoryChannel) {
+		return;
+	}else{
+		if(message.content.includes(" ") || message.content.length > 20){
+			// message.channel.bulkDelete(1, true).catch(err => {
+			// 	console.error(err);
+			// });
+			message.reply(`Invalid message ${cmonBruh}`)
+			.then(msg => {
+				msg.delete(10000)
+
+			})
+			.catch(err => {
+				console.error(err);
+			});
+		}
+	}
+
+	// auto-ban users by message
+	array1.forEach(function(item){
+		if(message.content.toLowerCase().includes(item)){
+			const member = message.guild.member(message.author);
+			if(member){
+				member.ban({
+					reason: 'Message',
+				}).catch(err => {
+					console.error(err);
+				});
+				const channel = message.guild.channels.find(channel => channel.name === "united-antichess-security-council");
+				if(!channel) return;
+				channel.send(`${message.author} has been banned for saying "${message}"`)
+			}
+		}
+	});
+
+
+	if (!message.content.startsWith(PREFIX)) return;
+
+		let args = message.content.substring(PREFIX.length).split(" ");
+
+		switch(args[0]){
+			case "ping":
+				clbot.commands.get('avatar').execute(message, args);
+		}
+//		const command = args.shift().toLowerCase();
+
+//		if (!clbot.commands.has(command)) return;
+
+//		try {
+//			clbot.commands.get(command).execute(message, args);
+//		} catch (error) {
+//			console.error(error);
+//			message.reply('there was an error trying to execute that command!');
+//		}
+	});
 
 	// Log our bot in using the token from https://discordapp.com/developers/applications/me
 	clbot.login(token);
